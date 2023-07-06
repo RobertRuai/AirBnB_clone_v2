1a1320bd0ece068a6c90c35badb6054c561104a5
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-06 12:28:56 +0000

    Bash script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 #sets up our web servers for the deployment of web_static
 
-
+html="
+<html>
+    <body>
+        <p>Holberton School</p>
+    </body>
+</html>
+"
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx start
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+printf "%s" "$html" >> /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\t autoindex off;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
