8bfe5b250640dfdbfe8a07a50bbedf90e7f83a71
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-06 21:18:23 +0000

    web_server edit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -16,5 +16,5 @@ sudo mkdir -p /data/web_static/shared/
 printf "%s" "$html" >> /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\t autoindex off;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
