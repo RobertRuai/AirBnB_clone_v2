dd8bbb04f0bfc78e15dc5fe948f9b4d7aeecc2f8
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-06 14:33:32 +0000

    1. Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """Fabric script creates,distributes archive to web-servers"""
-
 from fabric.api import local
 from datetime import datetime
 
-env.hosts = ['54.82.173.73', '54.237.69.144']
 
 def do_pack():
     """generates a tgz archive"""
@@ -14,5 +12,5 @@ def do_pack():
         f = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(f))
         return f
-    except:
+    except e:
         return None
