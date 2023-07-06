c80df14bf96b3c74b80c62c7348c5efbd10e6098
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-06 18:36:06 +0300

    fabric edit

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,6 +7,7 @@ def do_pack():
     """generates a tgz archive"""
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     try:
+        local("mkdir -p versions")
         f = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(f))
         return f
