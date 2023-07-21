0318b05cd30c94ebb56825f6b4208bfa7ffc3046
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-21 17:37:00 +0300

    n is a number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""hello_route module"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """entry function"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """displays hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """displays C followed by input text"""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pycool(text="is cool"):
+    """display “Python ”, followed by text"""
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """displays n if its a number"""
+    return str(n) + " is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """ display a HTML page only if n is an integer"""
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == "__main__":
+    """executes object code"""
+    app.run(host='0.0.0.0', port=5000)
