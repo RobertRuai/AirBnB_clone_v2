ff8780c64d221283ceaed21d75ef153f20674e91
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-21 16:44:53 +0300

    Python is cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,6 +23,7 @@ def c(text):
     """displays C followed by input text"""
     return "C " + text.replace('_', ' ')
 
+
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def pycool(text="is cool"):
