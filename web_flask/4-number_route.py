b4217e364db5a307d848d95026bcf9345e9bcf61
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-21 17:13:51 +0300

    n is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""hello_route module"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """entry function"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """displays hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """displays C followed by input text"""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pycool(text="is cool"):
+    """display “Python ”, followed by text"""
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """displays n if its a number"""
+    return str(n) + " is a number"
+
+
+if __name__ == "__main__":
+    """executes object code"""
+    app.run(host='0.0.0.0', port=5000)
